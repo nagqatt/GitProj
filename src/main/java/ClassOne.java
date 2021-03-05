1e5e38d816b4945a3f309e64aa60843c202492ed
nagqatt <nagqatt@yahoo.co.uk>
2021-03-05 22:22:59 +0000

    Jira-56 : Added another method

--- src/main/java/ClassOne.java
+++ src/main/java/ClassOne.java
@@ -2,6 +2,9 @@ public class ClassOne {
 
     public void testingthis(){
         System.out.println("testing this Number 1");
+    }
+    public void anothertestAdded(){
+        System.out.println("Added another test number 2");
 
     }
 
