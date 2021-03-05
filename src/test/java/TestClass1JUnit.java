0b94246d50a43290929c3992b8f2005e0e27da02
nagqatt <nagqatt@yahoo.co.uk>
2021-03-05 22:17:55 +0000

    Jira-456 : Uptate the code

--- src/test/java/TestClass1JUnit.java
+++ src/test/java/TestClass1JUnit.java
@@ -4,8 +4,13 @@ public class TestClass1JUnit {
 
     @Test
 
-    public void test1(){
+    public void test1() {
         System.out.println("This is JUnit");
+    }
+        @Test
+        public void addedAnotherTest(){
+           System.out.println("This is JUnit 2");
+        }
 
 
 
